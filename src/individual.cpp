9da5592b12af7238489b0043e56c83a7cfa1cbb8
knoellle <konrad@familie-noelle.de>
2022-02-16 00:18:43 +0100

    Add simple individual processing

--- src/individual.cpp
+++ src/individual.cpp
@@ -5,3 +5,14 @@ Individual::Individual() : life{1.0}, position{0.0, 0.0}, orientation{0.0} {};
 Individual::Individual(float life, Vector2 position, float orientation)
     : life{life}, position{position}, orientation{orientation} {};
 ;
+
+void Individual::process() {
+  outputs[0] = 0;
+  outputs[1] = 1.0;
+  for (int i = 0; i < 10; i++) {
+    outputs[0] += inputs[i] * (static_cast<float>(i) - 4.5);
+    outputs[1] -= abs(inputs[i] * (static_cast<float>(i) - 4.5));
+  }
+  outputs[0] /= 10.0;
+  outputs[1] /= 10.0;
+};
--- src/individual.hpp
+++ src/individual.hpp
@@ -8,6 +8,11 @@ public:
   float orientation;
   float clock_offset;
 
+  float inputs[10];
+  float outputs[2];
+
   Individual();
   Individual(float life, Vector2 position, float orientation);
+
+  void process();
 };
